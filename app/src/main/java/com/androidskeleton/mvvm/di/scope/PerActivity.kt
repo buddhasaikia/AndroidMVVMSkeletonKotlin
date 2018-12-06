c437b51d635d969f8ec76ae6cdb8c302ec6ba89e
Buddha Saikia <saikia.buddha@gmail.com>
2018-12-06 16:13:02 +0530

    java to kotlin conversion

---
+++ app/src/main/java/com/androidskeleton/mvvm/di/scope/PerActivity.kt
@@ -0,0 +1,12 @@
+package com.androidskeleton.mvvm.di.scope
+
+import javax.inject.Scope
+
+/**
+ * Created by Buddha Saikia on 05-06-2017.
+ */
+
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class PerActivity
+
